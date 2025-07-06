a000ae60a1d59c567ddfe8bcb794d4d896700b13
Sihang Li <sihang.li@duke.edu>
2025-07-06 16:22:50 -0700

    minor change

--- so_50kit_baseline.sh
+++ so_50kit_baseline.sh
@@ -7,14 +7,14 @@ images_file=('train.txt' 'test_syn.txt' 'train.txt')
 da=()
 
 python train_dpt_sourceonly.py \
---root_dir /mnt/data/SynRS3D/data \
+--root_dir /mnt/synrs3d/SynRS3D/data \
 --datasets ${train_set[*]} \
 --test_datasets ${test_set[*]} \
 --ood_datasets ${test_set[*]} \
 --crop_size 392 \
 --encoder vitl \
 --decoder DPT \
---snapshot_dir /mnt/data/SynRS3D/snapshot_so_baseline_50k\
+--snapshot_dir /mnt/synrs3d/SynRS3D/snapshot_so_baseline_50k\
 --images_file ${images_file[*]} \
 --batch_size 1 \
 --learning_rate 1e-6 \
