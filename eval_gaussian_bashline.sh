337b5e87db538c33efa4c5c729445a0550f231d1
Sihang Li <sihang.li@duke.edu>
2025-07-20 22:46:42 -0700

    update

---
+++ eval_gaussian_bashline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+test_set=('DFC18' 'DFC19_JAX' 'DFC19_OMA' 'geonrw_rural' 'geonrw_urban' 'OGC_ARG' 'OGC_ATL')
+
+python evaluation.py \
+--restore_path /mnt/synrs3d/SynRS3D/snapshot_so_gaussian_50k/DPT_vitl/392_lr_1e-06_wd_0.0005/multi_task_ori_class/best_HE_model_11500.pth \
+--test_datasets ${test_set[*]}  \
+--ood_datasets ${test_set[*]} \
+--pretrained \
+--gpu 0 \
+--images_file train.txt test.txt test.txt \
+--snapshot_dir snapshot_so_gaussian_50k_eval \
+--root_dir /mnt/synrs3d/SynRS3D/data \
+--batch_size 1 \
+--crop_size 392 \
+--encoder vitl \
+--decoder DPT \
+--multi_task
+
+# Note: OEM evaluation is not included as requested
