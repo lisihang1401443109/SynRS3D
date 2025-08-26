f6c7a6f7646e8705d8dda45ac15608c8e9fe8efe
Sihang Li <sihang.li@duke.edu>
2025-08-26 19:55:15 -0400

    123

--- autoalbument/install_autoalbument.sh
+++ autoalbument/install_autoalbument.sh
@@ -37,6 +37,7 @@ conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvi
 # Install AutoAlbument and dependencies
 echo -e "\n${GREEN}Installing AutoAlbument and dependencies...${NC}"
 pip install --upgrade pip
+pip install timm==0.4.12
 pip install autoalbument
 pip install albumentations==1.3.0
 pip install hydra-core --upgrade
