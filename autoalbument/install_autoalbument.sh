504c07673419ac2c1eb9f1df88bc709c466c1dac
Sihang Li <lisihang1401443109@gmail.com>
2025-09-02 18:22:13 -0400

    123

--- autoalbument/install_autoalbument.sh
+++ autoalbument/install_autoalbument.sh
@@ -33,7 +33,8 @@ conda activate $ENV_NAME
 
 # Install AutoAlbument and dependencies
 echo -e "\n${GREEN}Installing AutoAlbument and dependencies...${NC}"
-pip install --upgrade pip
+# pip install --upgrade pip
+python -m pip install --upgrade pip==1.24
 pip install -U autoalbument
 
 
