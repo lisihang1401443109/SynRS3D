7e9bb1b82a0b666a3100012a170b0d952364e4f7
Sihang Li <lisihang1401443109@gmail.com>
2025-09-21 01:12:01 -0400

    123

--- autoalbument/install_autoalbument.sh
+++ autoalbument/install_autoalbument.sh
@@ -47,7 +47,7 @@ pip install -U autoalbument
 echo -e "\n${GREEN}Installing required packages with specific versions...${NC}"
 pip install six
 # pip install torch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
-pip install torch==1.8.0+cu101 torchvision==0.9.0+cu10  1 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install timm==0.3.2
 pip install segmentation-models-pytorch==0.1.3
 pip install hydra-core==1.0.6
