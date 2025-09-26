4a8366d3b4c593eeea5571e760b2ca7bf3dfaf23
Sihang Li <lisihang1401443109@gmail.com>
2025-09-26 18:42:25 -0400

    123

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -151,6 +151,8 @@ class MultiTaskDataSet(data.Dataset):
                 ready_mask = [np.copy(mask) for mask in masks_to_transform if mask is not None]
                 #for mask in ready_mask:
                     # print('------masks strides: ', mask.strides)
+                # check the type of image
+                print('------image type: ', type(image))
                 augmented = self.transforms(image=image.cpu().numpy() if isinstance(image, torch.Tensor) else image, masks=ready_mask)
                 image = augmented['image']
                 transformed_masks = augmented['masks']
