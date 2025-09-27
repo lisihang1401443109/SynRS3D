254221f869e1e0c15068546e6566c99e4a6b2573
Sihang Li <lisihang1401443109@gmail.com>
2025-09-26 21:10:03 -0400

    123

--- so_autoalbument_50k.sh
+++ so_autoalbument_50k.sh
@@ -5,7 +5,7 @@ train_set=('grid_g05_mid_v2' 'grid_g005_mid_v2' 'terrain_g05_low_v1' 'terrain_g0
 test_set=('DFC18' 'DFC19_JAX' 'DFC19_OMA' 'geonrw_rural' 'geonrw_urban' 'OGC_ARG' 'OGC_ATL')
 
 # Path to the AutoAlbument policy
-POLICY_PATH="/mnt/synrs3d/SynRS3D/autoalbument/outputs/2025-09-09/01-31-02/policy/latest.json"
+POLICY_PATH="/mnt/synrs3d/SynRS3D/autoalbument/outputs/2025-09-09/01-31-02/policy/latest_392.json"
 
 # Training parameters
 python train_dpt_autoalbument.py \
