28948a0c6f1a75c7f727b6af1a928ae70c9487d2
Sihang Li <lisihang1401443109@gmail.com>
2025-10-19 04:41:43 -0400

    123

---
+++ autoalbument/cifar/dataset.py
@@ -0,0 +1,19 @@
+import cv2
+import torchvision
+
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+
+
+class Cifar10SearchDataset(torchvision.datasets.CIFAR10):
+    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+
+        return image, label
