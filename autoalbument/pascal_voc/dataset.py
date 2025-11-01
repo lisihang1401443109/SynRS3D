7170aa4c3c951d270dec8a110e33e30f38123541
Sihang Li <lisihang1401443109@gmail.com>
2025-11-01 06:11:52 -0400

    123

--- autoalbument/pascal_voc/dataset.py
+++ autoalbument/pascal_voc/dataset.py
@@ -94,6 +94,22 @@ class PascalVOCTrainDataset(VOCSegmentation):
     def __init__(self, root="/mnt/synrs3d/SynRS3D/autoalbument/pascal_voc/data", image_set="train", download=True, transform=None):
         super().__init__(root=root, image_set=image_set, download=download, transform=transform)
         
+    @staticmethod
+    def _convert_to_segmentation_mask(mask):
+        # This function converts a mask from the Pascal VOC format to the format required by AutoAlbument.
+        #
+        # Pascal VOC uses an RGB image to encode the segmentation mask for that image. RGB values of a pixel
+        # encode the pixel's class.
+        #
+        # AutoAlbument requires a segmentation mask to be a NumPy array with the shape [height, width, num_classes].
+        # Each channel in this mask should encode values for a single class. Pixel in a mask channel should have
+        # a value of 1.0 if the pixel of the image belongs to this class and 0.0 otherwise.
+        height, width = mask.shape[:2]
+        segmentation_mask = np.zeros((height, width, len(VOC_COLORMAP)), dtype=np.float32)
+        for label_index, label in enumerate(VOC_COLORMAP):
+            segmentation_mask[:, :, label_index] = np.all(mask == label, axis=-1).astype(float)
+        return segmentation_mask
+        
     def __getitem__(self, index):
         image = cv2.imread(self.images[index])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
