0f722ba7aed232d1528cac232dc2f7f6bf7b5cf2
Sihang Li <lisihang1401443109@gmail.com>
2025-11-08 19:30:47 -0500

    123

--- so_autoalbument_50k.sh
+++ so_autoalbument_50k.sh
@@ -25,7 +25,7 @@ python train_dpt_autoalbument.py \
 --decoder DPT \
 --snapshot_dir "$SNAPSHOT_DIR" \
 --images_file ${images_file[*]} \
---batch_size 1 \
+--batch_size 32 \
 --learning_rate 1e-6 \
 --weight_decay 5e-4 \
 --warmup_steps 0 \
