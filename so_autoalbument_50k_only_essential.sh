e497b0d792d0bb64bc96ddd966383eff768a6a98
Sihang Li <lisihang1401443109@gmail.com>
2025-11-10 23:46:00 -0500

    123

--- so_autoalbument_50k_only_essential.sh
+++ so_autoalbument_50k_only_essential.sh
@@ -25,7 +25,7 @@ python train_dpt_autoalbument_only_essential.py \
 --decoder DPT \
 --snapshot_dir "$SNAPSHOT_DIR" \
 --images_file ${images_file[*]} \
---batch_size 16 \
+--batch_size 8 \
 --learning_rate 1e-6 \
 --weight_decay 5e-4 \
 --warmup_steps 0 \
